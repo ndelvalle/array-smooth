eef0f2a3ac22df15028dac1a1d99507f24722b61
gillchristian <gillchristiang@gmail.com>
2018-07-05 00:01:07 +0200

    Add benchmarks

---
+++ benchmarks/map-implementation.js
@@ -0,0 +1,40 @@
+const dotProp = require('dot-prop')
+
+function getSample(arr, index, offset) {
+  const leftOffeset = index - offset
+  const from = leftOffeset >= 0 ? leftOffeset : 0
+  const to = index + offset + 1
+  return arr.slice(from, to)
+}
+
+function buildAccessors(accessor, accessorName) {
+  if (!accessor) {
+    return accessorName === 'get' ? (item) => item : undefined
+  }
+  if (typeof accessor === 'function') {
+    return accessor
+  }
+  if (typeof accessor === 'string') {
+    return (item, itemSmoothed) =>
+      dotProp[accessorName](item, accessor, itemSmoothed)
+  }
+  const accessorFullName = accessorName === 'set' ? 'setter' : 'getter'
+  throw new Error(`Error ${accessorFullName} must be a function or a string`)
+}
+
+function smooth(arr, smoothOffset, getter, setter) {
+  const get = buildAccessors(getter, 'get')
+  const set = buildAccessors(setter, 'set')
+
+  return arr.map((item, index, arr) => {
+    const sample = getSample(arr, index, smoothOffset).map(get)
+    const smoothed = sample.reduce((a, b) => a + b, 0) / sample.length
+    if (!set) {
+      return smoothed
+    }
+    set(item, smoothed)
+    return item
+  })
+}
+
+module.exports = smooth
